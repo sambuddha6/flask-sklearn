e30707128da505a34a775ed42fc1d0a298e89d17
Sambuddha Dasgupta <sambuddha6@gmail.com>
2022-10-13 22:50:47 +0530

    Update locustfile.py

--- locustfile.py
+++ locustfile.py
@@ -10,4 +10,5 @@ class QuickstartUser(HttpUser):
 
 
     def on_start(self):
-        self.client.post("/predict")
+        self.client.post("/predict", json = {"CHAS": {"0": 0}, "RM": {"0": 6.575}, "TAX": {"0": 296.0}, "PTRATIO": {"0": 15.3}, "B": {"0": 396.9},
+            "LSTAT": {"0": 4.98}})
